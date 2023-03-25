f10b43f9bc7359c9c630f4955e62d9570b6441f3
Zoruda <stronger6@gmail.com>
2023-03-25 13:54:12 -0600

    Cleanup

--- Assets/Scripts/ButtonHover.cs
+++ Assets/Scripts/ButtonHover.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
  
